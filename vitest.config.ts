76314eb9cc6da7ccbafc98b9c7cdb9728eaf5e68
wijan <me@wijan.dev>
2026-01-23 17:09:31 +0100

    Fixed vitest config in root

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
             'apps/*/vitest.config.ts',
             'packages/*/vitest.config.ts',
         ],
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'lcov'],
+        },
     },
 });
